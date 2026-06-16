2d717b5e0f6e3dcad568b44f502dd17fea8540c3
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-16 09:27:34 +0530

    Initial commit

--- src/BasicDSA/Sum.java
+++ src/BasicDSA/Sum.java
@@ -1,4 +1,20 @@
 package BasicDSA;
 
+import java.util.Scanner;
 public class Sum {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your first number:- ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter your Second number:- ");
+        int num2 = scan.nextInt();
+
+        int sum =(num1+num2);
+
+        System.out.println("Sum of two number is:- " +sum);
+
+        scan.close();
+    }
 }
