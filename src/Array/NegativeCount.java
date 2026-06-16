d7ae22ef26c018efdeb2588aa2edb343e85f3b1c
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-16 11:48:11 +0530

    Initial Commit

---
+++ src/Array/NegativeCount.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.Scanner;
+public class NegativeCount {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Size:- ");
+        int size = scan.nextInt();
+
+        if(size <= 0){
+            System.out.println("Array is empty");
+            return;
+        }
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter your Array elements:- ");
+        for(int i = 0; i<size; i++){
+            arr[i] = scan.nextInt();
+        }
+
+        //-----Negative count;
+        int negCount = 0;
+        for(int i = 0; i<size; i++){
+            if(arr[i] < 0){
+                negCount++;
+            }
+        }
+
+        System.out.println("Negative count is:- " +negCount);
+    }
+}
